6afc9cc02a4dd6468a1b15e09b09f8c6d4acc25e
Alvaro Marenco <alvaromarencom@gmail.com>
2024-09-10 19:10:13 -0600

    Migracion a Axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-async function trendingMoviesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-    const data = await res.json();
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+    },
+    params: {
+        'api_key': API_KEY,
+    },
+})
 
+
+async function trendingMoviesPreview() {
+    const {data} = await api('/trending/movie/day');
     const movies = data.results;
+    
     movies.forEach(movie => {
         const trendingMoviesPreviewcontainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
 
@@ -21,10 +31,9 @@ async function trendingMoviesPreview() {
 }
 
 async function getCategoriesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
-    const data = await res.json();
+    const {data} = await api('/genre/movie/list');
+    const categories = data.genres; 
 
-    const categories = data.genres;
     categories.forEach(category => {
         const previewCategoriescontainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
 
