26bed2e4c5427bb268f1a231a5463bcb79b54400
Alvaro Marenco <alvaromarencom@gmail.com>
2024-09-25 12:53:09 -0600

    Agregando imagenes por defecto

--- src/main.js
+++ src/main.js
@@ -36,6 +36,10 @@ function createMovies(movies, container, lazyLoad = false) {
         movieImg.setAttribute('alt', movie.title);
         movieImg.setAttribute(lazyLoad ? 'data-img' : 'src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
 
+        movieImg.addEventListener('error', () => {
+            movieImg.setAttribute('src', './Assets/imgNotFound.png');
+        });
+
         if(lazyLoad) {
             lazyLoader.observe(movieImg);
         };
@@ -114,7 +118,7 @@ async function getMoviesByCategory(id) {
     });
     const movies = data.results;
 
-    createMovies(movies, genericSection);
+    createMovies(movies, genericSection, true);
 };
 
 async function getMoviesBySearch(query) {
